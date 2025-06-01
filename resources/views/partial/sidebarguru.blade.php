e20c7316116bfb8332e0ddaff20ae844317f1733
CARLES MARVIN <charlesmarvin4321@gmail.com>
2025-06-01 19:38:12 +0700

    upgrade beberapa

--- resources/views/partial/sidebarguru.blade.php
+++ resources/views/partial/sidebarguru.blade.php
@@ -33,8 +33,12 @@
                             class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300"
                             aria-expanded="false" data-dropdown-toggle="dropdown-user">
                             <span class="sr-only">Open user menu</span>
-                            <img class="w-8 h-8 rounded-full"
-                                src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo">
+                            <div
+                                class="relative inline-flex items-center justify-center w-8 h-8 overflow-hidden bg-gradient-to-br from-blue-500 to-blue-600 rounded-full">
+                                <span class="font-medium text-sm text-white">
+                                    {{ Auth::guard('guru')->check() ? strtoupper(substr(Auth::guard('guru')->user()->name, 0, 1)) : '?' }}
+                                </span>
+                            </div>
                         </button>
                     </div>
                     <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-sm shadow-sm"
