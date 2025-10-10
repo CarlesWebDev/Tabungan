0319f37216f775105cf1f5ccdc754bb06d6491f3
CarlesWebDev <charlesmarvin4321@gmail.com>
2025-10-10 21:15:47 +0700

    chore:clean code

--- resources/views/LandingPage.blade.php
+++ resources/views/LandingPage.blade.php
@@ -739,7 +739,7 @@
                         <h3 class="text-lg font-semibold mb-4">Kontak Kami</h3>
                         <ul class="space-y-3 text-black">
                             <li class="flex items-start">
-                                <svg class="lucide lucide-map-pin h-5 w-5 text-blue-400 mr-3 mt-0.5"
+                                <svg class="lucide lucide-map-pin h-10 w-10 text-blue-400 mr-3 mt-0.5"
                                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                     fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                     stroke-linejoin="round">
